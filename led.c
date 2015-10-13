a43580fd3f070aa1ab6fe3c54b3554896c326487
whl <gggpwysov@126.com>
2015-10-13 11:24:38 +0800

    modified:   led.c 	1、将led灯驱动改为platform总线形式
        2015-10-13 @ whl

--- led.c
+++ led.c
@@ -1,38 +1,54 @@
 #include <linux/init.h>
-#include <linux/export.h>
-#include <linux/module.h>
-#include <linux/moduleparam.h>
+#include <linux/err.h>
 #include <linux/errno.h>
-#include <linux/miscdevice.h>
-#include <linux/types.h>
-#include <linux/io.h>
-#include <linux/fs.h>  
+#include <linux/mutex.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/of_platform.h>
+#include <linux/of_gpio.h>
+#include <linux/slab.h>
+#include <linux/workqueue.h>
+#include <linux/module.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
 #include <asm/gpio.h>
-
+#include <linux/klha_gpio.h>
+#include <asm/uaccess.h> //copy_to_user 头文件
 #define DEVICE_NAME "leds"
-#define GPIO_TO_PIN(bank, gpio) (32 * (bank) + (gpio))
 
-static unsigned long led_table[] = {
-	GPIO_TO_PIN(2, 12),  //gpio2_12, led zigbee
-	GPIO_TO_PIN(2, 13),  //gpio2_13, led net
-	GPIO_TO_PIN(2, 14),  //gpio2_14, led usb
-//	GPIO_TO_PIN(2, 10),  //gpio2_10, led sys,转接转上的led
+static int major = 0;
+static struct class * myleds_class;
+struct leds_data {
+	struct cdev			cdev;
+	dev_t				devt;
+	struct list_head	device_entry;
+	unsigned			users;
+	struct gpio_klha_platform_data	*data;
 };
 
+static LIST_HEAD(device_list);
+static DEFINE_MUTEX(device_leds_lock);
 
-static int am3352_leds_ioctl(
-		struct file *file,
-		unsigned int cmd,
-		unsigned long arg){
+static const struct of_device_id of_myleds_match[] = {
+	{.compatible = "myleds",},
+	{},
+};
+
+static int myleds_ioctl( struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	struct leds_data	*pdata;
+	pdata = filp->private_data;
+
 	switch(cmd){
 		case 0:	
-			gpio_direction_output(led_table[arg], 0);	
+			gpio_direction_output(pdata->data->gpio_array[arg], 0);	
 			//printk("gpio_set_value, port=%d, cmd=%d\n", led_table[arg], cmd);
 			//printk("==>gpio_get_value, port=%d, states=%d\n", led_table[arg], gpio_get_value(led_table[arg]));
 			return 0;
 			
 		case 1:
-			gpio_direction_output(led_table[arg], 1);
+			gpio_direction_output(pdata->data->gpio_array[arg], 1);	
 			//printk("gpio_set_value, port=%d, cmd=%d\n", led_table[arg], cmd);
 			//printk("==>gpio_get_value, port=%d, states=%d\n", led_table[arg], gpio_get_value(led_table[arg]));
 			return 0;
@@ -41,61 +57,157 @@ static int am3352_leds_ioctl(
 	} 
 }
 
-static struct file_operations dev_fops = {
-		.owner =     THIS_MODULE,
-		.unlocked_ioctl =     am3352_leds_ioctl,
-}; 
+static int myleds_open(struct inode *inode, struct file *filp)
+{
+	struct leds_data *pdata;
+	int status = -ENXIO;
 
-static struct miscdevice misc = {
-		.minor = MISC_DYNAMIC_MINOR,
-		.name  = DEVICE_NAME,
-		.fops  = &dev_fops,
-};
-
-static int __init dev_init(void){
-	int ret;
-	int ret2=0;
-	/*
-	ret2 = gpio_request(GPIO_TO_PIN(2, 10), "gpio2_10");
-	if(ret2!=0) {
-		printk("gpio_request port(2, 10) error %d.\n", ret2);
-	}*/
-	ret2 = gpio_request(GPIO_TO_PIN(2, 12), "gpio2_12");
-	if(ret2!=0) {
-		printk("gpio_request port(2, 12) error %d.\n", ret2);
-	}
-	ret2 = gpio_request(GPIO_TO_PIN(2, 13), "gpio2_13");
-	if(ret2!=0) {
-		printk("gpio_request port(2, 13) error %d.\n", ret2);
-	}
-	ret2 = gpio_request(GPIO_TO_PIN(2, 14), "gpio2_14");
-	if(ret2!=0) {
-		printk("gpio_request port(2, 14) error %d.\n", ret2);
+	mutex_lock(&device_leds_lock);
+	list_for_each_entry(pdata, &device_list, device_entry) {
+		if(pdata->devt == inode->i_rdev) {
+			status = 0;
+			break;
+		}
 	}
 
-	ret = misc_register(&misc);
-	if(ret < 0) {
-		printk(DEVICE_NAME ":can't register device.");
+	if(status == 0) {
+		pdata->users++;
+		filp->private_data = pdata;
 	}
 
-	printk(DEVICE_NAME"\tinitialized\n");
-	
-	return ret;
+	mutex_unlock(&device_leds_lock);
+
+	return status;
 }
 
-static void __exit dev_exit(void){
-	//gpio_free(GPIO_TO_PIN(2, 10));
-	gpio_free(GPIO_TO_PIN(2, 12));
-	gpio_free(GPIO_TO_PIN(2, 13));
-	gpio_free(GPIO_TO_PIN(2, 14));
-	//gpio_direction_output(GPIO_TO_PIN(2, 10),0);
-	//gpio_direction_output(GPIO_TO_PIN(0, 17),0);
-	//gpio_direction_output(GPIO_TO_PIN(1, 8),0);
+static int myleds_release(struct inode *inode, struct file *filp)
+{
+	struct leds_data	*pdata;
+	pdata = filp->private_data;
 
-	misc_deregister(&misc);
+	mutex_lock(&device_leds_lock);
+	pdata->users--;
+	mutex_unlock(&device_leds_lock);
+
+	return 0;
+}
+	
+
+static struct file_operations dev_fops = {
+		.owner =     THIS_MODULE,
+		.unlocked_ioctl =     myleds_ioctl,
+		.open	=	myleds_open,
+		.release	= myleds_release,
+}; 
+
+static void myleds_setup_cdev(struct leds_data *pdata, int index)
+{
+	int err, devno = MKDEV(major, index);
+	cdev_init(&pdata->cdev, &dev_fops);
+
+	err = cdev_add(&pdata->cdev, devno, 1);
+	if(err)
+		printk(KERN_NOTICE "Error %d adding leds\n", err);
+}
+
+static int __devinit myleds_probe(struct platform_device *pdev)
+{
+	int status;
+	struct leds_data * pdata;
+	pdata = kzalloc(sizeof(*pdata), GFP_KERNEL);
+	if(!pdata)
+		return -ENOMEM;
+
+	status = alloc_chrdev_region(&pdata->devt, 0, 1, DEVICE_NAME);
+	if(status < 0)
+		return status;
+	major = MAJOR(pdata->devt);
+
+	myleds_setup_cdev(pdata, 0);
+
+	pdata->data = pdev->dev.platform_data;
+	if(!pdata->data) {
+		printk(KERN_ERR "NOT FIND GPIO.");
+		return -ENODEV;
+	}
+
+	//打印管脚信息
+	int i=0;
+	for(i = 0; i<pdata->data->nums; i++) {
+		gpio_request(pdata->data->gpio_array[i], NULL);
+		printk(KERN_NOTICE "led%d gpio=>%d\n", i, pdata->data->gpio_array[i]);
+	}
+
+	/*创建一个设备节点，名为/dev/leds */
+	struct device *dev;
+	dev = device_create(myleds_class, &pdev->dev, pdata->devt, pdata, "leds");
+	status = IS_ERR(dev) ?PTR_ERR(dev) : 0;
+	
+	if (status == 0) {
+		list_add(&pdata->device_entry, &device_list);
+		platform_set_drvdata(pdev, pdata);	
+	} else {
+		kfree(pdata);
+	}
+	
+	return status;
+}
+
+static int __devexit myleds_remove(struct platform_device *pdev)
+{
+	struct leds_data *pdata;
+	pdata = platform_get_drvdata(pdev);
+	cdev_del(&pdata->cdev);
+	device_destroy(myleds_class, pdata->devt);
+	unregister_chrdev_region(pdata->devt, 1);
+	printk("pdata->users=>%d\n", pdata->users);
+	if(pdata->users == 0) {
+		int i;
+		for(i=0; i<pdata->data->nums ; i++) {
+			gpio_free(pdata->data->gpio_array[i]);
+		}
+		kfree(pdata);
+	}
+}
+
+static struct platform_driver myleds_driver = {
+	.probe = myleds_probe,
+	.remove = __devexit_p(myleds_remove),
+	.driver = {
+		.name = "myleds",
+		.owner = THIS_MODULE,
+		.of_match_table = of_myleds_match,
+	},
+};
+
+static int __init dev_init(void)
+{
+	int status;
+
+	myleds_class = class_create(THIS_MODULE, "myleds");
+	if (IS_ERR(myleds_class)) {
+			return PTR_ERR(myleds_class);
+	}
+	status = platform_driver_register(&myleds_driver);
+
+	if(status < 0 ) {
+		printk(KERN_ERR "register myleds to platform failed!\n");
+		class_destroy(myleds_class);
+	}else
+		printk(KERN_NOTICE "myleds driver register success.\n");
+
+	return status;
+}
+
+static void __exit dev_exit(void)
+{
+	platform_driver_unregister(&myleds_driver);
+	class_destroy(myleds_class);
+	printk(KERN_NOTICE "myleds driver unregister success.\n");
 }
 
 module_init(dev_init);
 module_exit(dev_exit);
+MODULE_DESCRIPTION("led driver.");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("WangHeLou");
